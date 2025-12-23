90ba1bdbd5d39887bbfda76600d1d867d1afdb58
srikanth-pamidi <pamidisrikanth534@gmail.com>
2025-12-23 10:48:42 +0530

    List example

---
+++ src/net/konic/corejava/colletions/ListExample.java
@@ -0,0 +1,14 @@
+package net.konic.corejava.colletions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+	public static void main(String[] args) {
+		List list=new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		System.out.println("Daat :"+list);
+	}
+
+}
